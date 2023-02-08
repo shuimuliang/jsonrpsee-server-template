d036cda16aa6a6dc4b20462ee597f0dbd8fa636a
shuimuliang <shuimuliang@gmail.com>
2023-02-08 15:46:32 +0800

    fix: add simple minimal logger(simplelog)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use jsonrpsee::server::{RpcModule, ServerBuilder};
 use log::info;
+use simplelog::{TermLogger, LevelFilter, Config, TerminalMode, ColorChoice};
 use std::net::SocketAddr;
 use tokio::signal::ctrl_c;
 use tokio::signal::unix::{signal, SignalKind};
 
 #[tokio::main]
 async fn start() -> anyhow::Result<()> {
+    let _ = TermLogger::init(
+        LevelFilter::Trace,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    );
+
     let server = ServerBuilder::default()
         .build("127.0.0.1:3030".parse::<SocketAddr>()?)
         .await?;
